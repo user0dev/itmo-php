d6e595abd037ef80755bdb8480ce3118ec3558de
user0dev <user0dev@yandex.com>
2017-01-13 19:57:48 +0300

    complate job4

--- lesson2/job4.php
+++ lesson2/job4.php
@@ -1,8 +1,18 @@
 <?php
-    $binPattern = '[01]{1,12}';
+    $binPattern = '[01]{1,16}';
+    $decPattern = '/\d{1,6}/';
+    $valBin = '';
+    $valDec = '';
+    if (isset($_GET['to_bin']) && preg_match($decPattern, $_GET['dec']) == 1) {
+        $valDec = (int) $_GET['dec'];
+        $valBin = decbin($valDec);
+    } elseif (isset($_GET['to_dec']) && preg_match('/' . $binPattern . '/', $_GET['bin']) == 1) {
+        $valBin = $_GET['bin'];
+        $valDec = bindec($valBin);
+    }
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ru">
 <head>
     <meta charset="UTF-8">
     <title>Задача 4. Преобразовать из десятичной системы счисления в двоичную и наоборот</title>
@@ -24,11 +34,11 @@
     <form>
         <div><label>
             <span>Десятичная</span>
-            <input type="number">
+            <input type="number" name="dec" value="<?= $valDec ?>">
         </label></div>
         <div><label>
             <span>Двоичная</span>
-            <input type="text" pattern="<?= $binPattern ?>">
+            <input type="text" name="bin" pattern="<?= $binPattern ?>" value="<?= $valBin ?>">
         </label></div>
         <input type="submit" value="Перевести в двоичную" name="to_bin">
         <input type="submit" value="Перевести в десятичную" name="to_dec">
