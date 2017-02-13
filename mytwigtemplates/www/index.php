8a004e36df2ae869ab20218c34aba2a092e31bf4
user0dev <user0dev@yandex.com>
2017-02-13 17:29:12 +0300

    tried to inherit templates, tried to make bootstrap pages, added bootstrap blog template

--- mytwigtemplates/www/index.php
+++ mytwigtemplates/www/index.php
@@ -10,9 +10,9 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 $loader = new Twig_Loader_Filesystem(__DIR__ . "/../src/Templates");
 
-$twig = new Twig_Environment($loader, array('cache' => __DIR__ . '/../cache'));
+$twig = new Twig_Environment($loader, array('cache' => __DIR__ . '/../cache', 'debug' => true));
 
-$template = $twig->load("test.twig.html");
+$template = $twig->load("base.twig.html");
 
 $lorem = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias asperiores, aut, beatae delectus deserunt dignissimos doloribus illo iure laudantium molestiae obcaecati omnis possimus quaerat quia, reiciendis sapiente tempora temporibus totam.";
 
